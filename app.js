10c67f822bbefcf0bdfefa660ce8e21ca269898e
Khaya R. Dillon <yahyal0v3@yahoo.com>
2019-02-27 21:35:15 -0500

    Decapitalize game function arguments and related references, upcase tie, win, and lose function returns, change html ids to hyphenated words, and add computer player h3 tag

--- app.js
+++ app.js
@@ -2,19 +2,19 @@
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
 const scissors = document.getElementById("scissors")
-const outcomeText = document.getElementById("outcomeText")
+const outcomeText = document.getElementById("outcome-text")
 
 function gameListeners() {
 	rock.addEventListener("click", function() {
-		game("Rock")
+		game("rock")
 	})
 	
 	paper.addEventListener("click", function() {
-		game("Paper")
+		game("paper")
 	})
 	
 	scissors.addEventListener("click", function() {
-		game("Scissors")
+		game("scissors")
 	})
 }
 
@@ -24,43 +24,42 @@ function game(userPlayer) {
 	let compPlayer = generateHandsign()
 	
 	switch (`${userPlayer} vs ${compPlayer}`) {
-		case "Rock vs Rock":
-		case "Paper vs Paper":
-		case "Scissors vs Scissors":
+		case "rock vs rock":
+		case "paper vs paper":
+		case "scissors vs scissors":
 			tie(compPlayer);
 			break;
-		case "Rock vs Scissors":
-		case "Paper vs Rock":
-		case "Scissors vs Paper":
+		case "rock vs scissors":
+		case "paper vs rock":
+		case "scissors vs paper":
 			win(userPlayer, compPlayer);
 			break;
-		case "Rock vs Paper":
-		case "Paper vs Scissors":
-		case "Scissors vs Rock":
+		case "rock vs paper":
+		case "paper vs scissors":
+		case "scissors vs rock":
 			lose(userPlayer, compPlayer);
 			break;
 	}
-
 }
 
 function generateHandsign() {
-	const handsigns = ["Rock", "Paper", "Scissors"]
+	const handsigns = ["rock", "paper", "scissors"]
 	let randomIndex = Math.floor(Math.random() * 3)
 	return handsigns[randomIndex]
 }
 
 function tie(compHandsign) {
-	outcomeText.innerHTML = `Computer Player chose ${compHandsign}. It's a tie!`
+	outcomeText.innerHTML = `Computer Player chose ${compHandsign}. It's a tie!`.toUpperCase()
 }
 
 function win(userHandsign, compHandsign) {
-	outcomeText.innerHTML = `${userHandsign} ${attackVerb(userHandsign)} ${compHandsign}. You win!!`
+	outcomeText.innerHTML = `${userHandsign} ${attackVerb(userHandsign)} ${compHandsign}. You win!!`.toUpperCase()
 }
 
 function lose(userHandsign, compHandsign) {
-	outcomeText.innerHTML = `${compHandsign} ${attackVerb(compHandsign)} ${userHandsign}. You lose...`
+	outcomeText.innerHTML = `${compHandsign} ${attackVerb(compHandsign)} ${userHandsign}. You lose...`.toUpperCase()
 }
 
 function attackVerb(winner) {
-	return winner === "Rock" ? "smashes" : winner === "Paper" ? "covers" : "cuts"
+	return winner === "rock" ? "smashes" : winner === "paper" ? "covers" : "cuts"
 }

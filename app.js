7a1fbf1b71db7b441eb4a2764985c2d663d379e0
Khaya R. Dillon <yahyal0v3@yahoo.com>
2019-04-18 16:01:30 -0400

    Add gradient background to handsign divs and fix glow animation

--- app.js
+++ app.js
@@ -116,6 +116,6 @@ function attackVerb(winner) {
 function endGame() {
 	location.href = "#handsigns"
 	document.querySelectorAll('.selected').forEach(ele => ele.classList.remove('selected'))
-	$('#handsigns div').each((index, ele) => $(ele).on("click", gameListeners()))
+	$('#handsigns div').each((index, ele) => $(ele).on("click", gameListeners))
 	
 }
